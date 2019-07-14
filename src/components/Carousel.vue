e3c3291bcb7f29f1a21adae1dc2da298d75ec890
sydenny <1395854149@qq.com>
2019-07-14 09:56:38 +0800

    update carousel:adapt view

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="carousel">
     <div class="slide-list">
+      <!-- <div class="window"></div> -->
       <ul :style="'left: '+left + 'px'" class="moved">
         <li v-for="(item,index) in banners" :key="index">
           <a href="#">
-            <img :src="item" alt="baidu" class="img-size">
+            <img :src="item" alt="baidu" class="img-size" :style="`width:${viewWidth}px`" />
           </a>
         </li>
       </ul>
@@ -12,13 +13,11 @@
     <div class="slide-dots">
       <ul class="dots-pos">
         <li
-          v-for="(item,index) in 5"
+          v-for="(item,index) in banners.length"
           :key="index"
           :class="['dots-style',selected === index? 'selected':'']"
           @click="switchSlide(index)"
         ></li>
-        <!-- <li :class="['dots-style',selected === 1? 'selected':'']"></li> -->
-        <!-- <li :class="['dots-style',selected === 2? 'selected':'']"></li> -->
       </ul>
     </div>
   </div>
@@ -33,29 +32,27 @@ export default {
       ismove: false,
       timer: null,
       timeout: null,
+      view: document.body.clientWidth,
       banners: [
         "http://t16img.yangkeduo.com/pdd_oms/2019-05-05/fb23680a31aab00691bb772e07dc037e.jpg?imageMogr2/format/webp/quality/50",
         "http://t00img.yangkeduo.com/goods/images/2019-06-13/4aad05f1649635a549a35b556a65f7cc.jpeg?imageMogr2/format/webp/quality/50",
         "http://t00img.yangkeduo.com/goods/images/2019-06-12/744dc44bce6c40097c9e1693ac40183c.jpeg?imageMogr2/format/webp/quality/50",
         "http://t16img.yangkeduo.com/pdd_oms/2019-02-01/5c9476b93b15a1d003f53dbb14e81fcc.jpg?imageMogr2/format/webp/quality/50",
         "http://t16img.yangkeduo.com/pdd_oms/2019-05-05/fb23680a31aab00691bb772e07dc037e.jpg?imageMogr2/format/webp/quality/50"
-
-        // require("@/assets/carousel/01.jpg"),
-        // require("@/assets/carousel/02.jpg"),
-        // require("@/assets/carousel/03.jpg"),
-
-        // require("@/assets/carousel/02.jpg"),
-        // require("@/assets/carousel/03.jpg")
       ]
     };
   },
-  computed: {},
+  computed: {
+    viewWidth: function() {
+      return this.view;
+    }
+  },
   methods: {
     switchSlide(id) {
       clearTimeout(this.timeout);
       clearInterval(this.timer);
       this.selected = id;
-      this.left = -(id * 375);
+      this.left = -(id * this.view);
       this.ismove = !this.ismove;
       // console.log(this.left);
       this.timeout = setTimeout(() => {
@@ -69,12 +66,15 @@ export default {
         if (this.selected > 4) {
           this.selected = 0;
         }
-        this.left = -(this.selected * 375);
+        this.left = -(this.selected * this.view);
       }, 2000);
     }
   },
   mounted() {
     this.setTime();
+    window.onresize = () => {
+      this.view = window.document.body.clientWidth;
+    };
   }
 };
 </script>
@@ -83,8 +83,7 @@ $size: 8rem;
 .carousel {
   position: relative;
   width: 100%;
-  height: $size;
-  line-height: $size;
+
   text-align: center;
   .slide-dots {
     position: absolute;
@@ -115,20 +114,27 @@ $size: 8rem;
       transition: 0.6s;
     }
     ul {
-      height: $size;
       width: 1200%;
-      position: absolute;
+      position: relative;
       li {
-        width: 23.5rem;
+        // width: 23.5rem;
         float: left;
         // display: inline-block;
         a {
           .img-size {
-            width: 100%;
-            height: $size;
+            // width: 100%;
+            // height: $size;
           }
         }
       }
+    }
+    ul::after {
+      content: ".";
+      display: block;
+      visibility: hidden;
+      clear: both;
+      height: 0;
+      overflow: hidden;
     }
   }
 }
